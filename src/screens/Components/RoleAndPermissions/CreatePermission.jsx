edd7155ecbdaf55d8fc6f0e3350bfb184f445ae6
kadirtetik1 <tetik.kadir@hotmail.com>
2025-06-16 16:25:21 +0300

    update

--- src/screens/Components/RoleAndPermissions/CreatePermission.jsx
+++ src/screens/Components/RoleAndPermissions/CreatePermission.jsx
@@ -12,16 +12,7 @@ export default class CreatePermission extends Component {
       title: { label: 'Bağlı Olduğu Üst Başlık', value: '', type: 'text' },
       subTitle: { label: 'Bağlı Olduğu Alt Başlık', value: '', type: 'text' },
       routePath: { label: 'Page Route', value: '', type: 'text' },
-      type: {
-        label: 'Buton/Sayfa',
-        value: '',
-        type: 'dropdown',
-        options: [
-          { label: 'Sayfa', value: 'Sayfa' },
-          { label: 'Buton', value: 'Buton' }
-        ]
-      },
-      buttonKey: { label: 'Buton Keyword', value: '', type: 'text' }
+     
     };
 
     return (
