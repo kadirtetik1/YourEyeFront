b67f5bac58bcecc80494843ba4c6386374a83635
kadirtetik1 <tetik.kadir@hotmail.com>
2025-06-19 23:13:37 +0300

    docker updates

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,2 +1,5 @@
 
-export const apiBaseUrl = 'http://localhost:5059/api';
+
+// export const apiBaseUrl = 'http://localhost:8080/api';
+// export const apiBaseUrl = 'http://192.168.x.x:8080/api'; // local IP üzerinden test için
+ export const apiBaseUrl = 'http://backend:8080/api'; // docker için
